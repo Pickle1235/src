00faffaf89adca849b0d0423905baf5e865f9522
Pickle1235 <42522092+Pickle1235@users.noreply.github.com>
2018-11-11 16:25:51 -0500

    Add files via upload

---
+++ src/ChatFilter.java
@@ -0,0 +1,21 @@
+/**
+ * ChatFilter
+ *
+ * Filter for chat
+ *
+ * @author Geon An, Arron Smith
+ *
+ * @version November, 2018
+ *
+ */
+
+public class ChatFilter {
+
+    public ChatFilter(String badWordsFileName) {
+
+    }
+
+    public String filter(String msg) {
+        return msg;
+    }
+}
---
+++ src/ChatMessage.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+/**
+ * ChatMessage
+ *
+ * Message for chat
+ *
+ * @author Geon An, Arron Smith
+ *
+ * @version November, 2018
+ *
+ */
+final class ChatMessage implements Serializable {
+    private static final long serialVersionUID = 6898543889087L;
+
+    // Here is where you should implement the chat message object.
+    // Variables, Constructors, Methods, etc.
+}
